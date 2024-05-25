032d64d941a1f30e2513833fab3d43e51a55a9ca
chase104 <62479547+chase104@users.noreply.github.com>
2024-05-25 13:32:54 -0500

    Setup initial project - Install dependencies - Setup project structure - Setup version control - Setup SASS & Bootstrap integration

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="pt-4 ps-3 text-primary primary">Test Bootstrap & SCSS</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./scss/bootstrap.scss";
+import "./scss/font-awesome.scss";
+import "./scss/styles.scss";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
