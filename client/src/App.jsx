d128c65567a457a05439e880daf29034fd2b90b0
chase104 <62479547+chase104@users.noreply.github.com>
2024-05-25 14:10:39 -0500

    Setup Babel for transpiling into older js for browser compatibility

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 
 function App() {
+  const testState = {
+    message: "Test Bootstrap & SCSS",
+  };
   return (
     <>
-      <h1 className="pt-4 ps-3 text-primary primary">Test Bootstrap & SCSS</h1>
+      <h1 className="pt-4 ps-3 text-primary primary">{testState?.message}</h1>
     </>
   );
 }
